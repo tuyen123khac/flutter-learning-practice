bb392368e5ff23b4c21248f3e9614fc7b456eae1
Vo Khac Tuyen <vo.khactuyen@gmail.com>
2023-02-11 18:44:27 +0700

    Test drift basic

---
+++ lib/data/app_database.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:flutter_learning_practice/data/tables/categories.dart';
+import 'package:flutter_learning_practice/data/tables/todos.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [Todos, Categories])
+class AppDatabase extends _$AppDatabase {
+  // we tell the database where to store the data with this constructor
+  AppDatabase() : super(_openConnection());
+
+  // you should bump this number whenever you change or add a table definition.
+  // Migrations are covered later in the documentation.
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase.createInBackground(file);
+  });
+}
---
+++ lib/data/tables/categories.dart
@@ -0,0 +1,10 @@
+import 'package:drift/drift.dart';
+
+// This will make drift generate a class called "Category" to represent a row in
+// this table. By default, "Categorie" would have been used because it only
+//strips away the trailing "s" in the table name.
+@DataClassName('Category')
+class Categories extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get description => text()();
+}
